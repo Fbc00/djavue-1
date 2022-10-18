7a16231c6bf41020f28b0c8e7b318238f24d814c
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-10-18 01:29:31 -0300

    backend: added database connection

--- djavue/settings.py
+++ djavue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,9 +76,13 @@ WSGI_APPLICATION = "djavue.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME", default="djavue"),
+        "USER": config("DB_USER", default="djavue"),
+        "PASSWORD": config("DB_PASSWORD", default="djavue"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
+    },
 }
 
 
