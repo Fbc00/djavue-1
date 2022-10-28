c558dd94ad71bdeef8d555603e92011571e50560
joao.procopio <joao.procopio@buser.com.br>
2022-10-28 20:05:14 -0300

    linter: change linting rules

---
+++ frontend/prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleAttributePerLine: true,
+  vueIndentScriptAndStyle: true,
+  arrowParens: "always",
+  trailingComma: "es5",
+  tabWidth: 2,
+}
